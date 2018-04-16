b7c16ea94bf834ab36ce36c16619569b07491b2e
osapon <osa@osa-p.net>
2018-04-16 23:24:10 +0900

    refs #3

---
+++ widgets/overlay_shortcut_guide.php
@@ -0,0 +1,18 @@
+<div class="overlay_shortcut_guide invisible">
+  <header class="overlay_shortcut_guide_header">
+    <span><?=_('Shortcut key')?></span>
+  </header>
+  <div class="overlay_shortcut_guide_form">
+    <table>
+      <tr><td><span class="shortcut_button">n</span></td><td><?=_('Open the toot form')?></td></tr>
+      <tr><td><span class="shortcut_button">/</span></td><td><?=_('Move focus to search field')?></td></tr>
+      <tr><td><span class="shortcut_button">Ctrl + Enter</span></td><td><?=_('Toot in toot form')?></td></tr>
+      <tr><td><span class="shortcut_button">Shift + h</span></td><td><?=_('Switch to home time line')?></td></tr>
+      <tr><td><span class="shortcut_button">Shift + l</span></td><td><?=_('Switch to local time line')?></td></tr>
+      <tr><td><span class="shortcut_button">Shift + f</span></td><td><?=_('Switch to federated time line')?></td></tr>
+      <tr><td><span class="shortcut_button">Shift + n</span></td><td><?=_('Switch to notifications')?></td></tr>
+      <tr><td><span class="shortcut_button">Shift + v</span></td><td><?=_('Switch to favourites')?></td></tr>
+      <tr><td><span class="shortcut_button">?</span></td><td><?=_('Open this guide')?></td></tr>
+    </table>
+  </div>
+</div>
